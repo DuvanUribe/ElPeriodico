a4817767da9ad11ef65ebb0750c5912484ebc504
oleguiza <oleguiza@bancolombia.com.co>
2024-06-13 16:31:05 -0500

    version 1.01

--- src/main/java/uptc/frw/el_periodico/ElPeriodicoApplication.java
+++ src/main/java/uptc/frw/el_periodico/ElPeriodicoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ElPeriodicoApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ElPeriodicoApplication.class, args);
     }
 

593adfadcfeea427de7b0f907fe59a1f0610683a
Duvan <duvanygabi@gmail.com>
2024-06-13 14:35:38 -0500

    version

---
+++ src/main/java/uptc/frw/el_periodico/ElPeriodicoApplication.java
@@ -0,0 +1,13 @@
+package uptc.frw.el_periodico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElPeriodicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElPeriodicoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uptc/frw/el_periodico/ElPeriodicoApplicationTests.java
@@ -0,0 +1,13 @@
+package uptc.frw.el_periodico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElPeriodicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

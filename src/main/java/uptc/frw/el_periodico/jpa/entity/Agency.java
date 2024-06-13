ec6c0f0105bf3a3d56d9e63287bc5177953a162e
Sebas Ramos <sebasramos1996@gmail.com>
2024-06-13 16:32:36 -0500

    creacion paquetes y entudad Agency

---
+++ src/main/java/uptc/frw/el_periodico/jpa/entity/Agency.java
@@ -0,0 +1,33 @@
+package uptc.frw.el_periodico.jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "AGENCIA")
+public class Agency {
+
+    @Id
+    @Column(name = "ID_AGENCIA")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seqAgency")
+    @SequenceGenerator(name = "seqAgency",sequenceName = "SEQ_AGENCIA",allocationSize = 1)
+    private int id;
+
+    @Column(name ="NOMBRE_AGENCIA")
+    private String agencyName;
+
+    @Column(name ="ANIO_CREACION")
+    private Integer yearCreacion;
+
+   /* @OneToMany(mappedBy ="agency")
+    private List<AgencyNews> agencyNews;*/
+}
